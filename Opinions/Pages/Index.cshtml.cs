22c42ebedb81b96a8a57ade8c4d2320a33a39254
MichaelZp <michael.zappe@yahoo.de>
2022-07-22 18:04:13 +0200

    init

---
+++ Opinions/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Opinions.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Timestamp"] = DateTime.Now.ToString("dd.MM.yy hh:mm");
+        }
+    }
+}
---
+++ Opinions/Pages/NewPoll.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Opinions.Pages
+{
+    public class NewPollModel : PageModel
+    {
+        private readonly ILogger<NewPollModel> _logger;
+
+        public NewPollModel(ILogger<NewPollModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Timestamp"] = DateTime.Now.ToString("dd.MM.yy hh:mm");
+        }
+    }
+}
